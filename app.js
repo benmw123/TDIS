c9d6e05fab46904c914f637f2cbd6b4d35225cdf
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-10-18 15:36:52 -0500

    promise.all

--- app.js
+++ app.js
@@ -12,8 +12,22 @@ function formatParams(params) {
     return queryItems.join('&');
 }
 
+function fetchAndCatch(url) {
+    return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        //.then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    }
+
 function getApod(today) {
-    //get request for APOD API
+
     const params = {
         date: today,
         api_key: apiKey,
@@ -22,42 +36,27 @@ function getApod(today) {
     const queryString = formatParams(params)
     const url = searchURL + "/planetary/apod?" + queryString;
 
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
-
+    return fetchAndCatch(url);
 }
-//getApod(selectedDate);
 
-function getNeoWs(today) {  
+function getNeoWs(today) {
     const params = {
-    end_date: today,
-    api_key: apiKey,
+        start_date: today,
+        end_date: today,
+        api_key: apiKey,
+    }
+
+    const queryString = formatParams(params)
+    const url = searchURL + "/neo/rest/v1/feed?" + queryString;
+
+    return fetchAndCatch(url); 
 }
 
-const queryString = formatParams(params)
-const url = searchURL + "/neo/rest/v1/feed?" + queryString;
-
-fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-    //get request for NeoWs API
+function getFetchRequests(selectedDate) {
+    const apodFetch = getApod(selectedDate);
+    const neowsFetch = getNeoWs(selectedDate);
+    Promise.all([apodFetch, neowsFetch])
+        .then(promiseAll => displayResults(promiseAll, selectedDate));
 }
 
 function getInsight() {
@@ -68,36 +67,44 @@ function EarthToSOL() {
     //function may be needed to convert earth day to SOL day for Insight API
 }
 
-function displayResults(responseJson) {
-    console.log(responseJson); 
+function displayResults([apodData, neowsData], selectedDate) {
     $("#aPOD-results").empty();
     $("#aPOD-results").append(`
-                    <h3>${responseJson.title}<h3>
-                    <img src=${responseJson.url} alt="Astronomy Picture of the Day)">`);
-    $("#results").removeClass("hidden");                 
+                    <h3>Astronomy Picture of The Day</h3>
+                    <p>${apodData.title}</p>
+                    <img src=${apodData.url} alt="Astronomy Picture of the Day"></img>`);
+    $("#neoWs-results").empty();                 
+    $("#neoWs-results").append(`
+                    <h3>Near Earth Object Alert!</h3>
+                    <p>The asteroid ${neowsData.near_earth_objects[selectedDate][0].name.replace("(",'').replace(")","")} is traveling 
+                    at a relative speed of ${neowsData.near_earth_objects[selectedDate][0].close_approach_data[0].relative_velocity.miles_per_hour} miles per hour,
+                    and is approximately ${neowsData.near_earth_objects[selectedDate][0].close_approach_data[0].miss_distance.miles} miles from earth! </p>`)
+    $("#insight-results").empty();                 
+    $("#results").removeClass("hidden");
 }
 
-$("#date-picker").flatpickr({
-    enableTime: false,
-    dateFormat: "Y-m-d",
-    defaultDate: "today",
-    maxDate: "today",
-    minDate: "2020-01-01"
-
-}); 
-
 function watchCalender() {
-    $("#date-picker-js").click( function() {
-        const selectedDate = $("#date-picker").val(); 
-        getApod(selectedDate); 
-        //getInsight(selectedDate);
-        getNeoWs(selectedDate);
+    $("#date-picker").flatpickr({
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        defaultDate: "today",
+        maxDate: "today",
+        minDate: "2020-01-01"
+
     });
-    //function sets up for date selection on calender
+
+    $("#date-picker-js").click(function () {
+        const selectedDate = $("#date-picker").val();
+        getFetchRequests(selectedDate);
+    });
 }
 
 function hideResults() {
-    //a function that hides results based on user request 
+//a function that hides results based on user request 
+}
+
+function isVideo() {
+//need function to determine if Apod Json object url is a youtube link or img. Consider using .includes, or .endsWith 
 }
 
 $(watchCalender); 
